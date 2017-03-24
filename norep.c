cb2e3c5218fd52b6bbae3c3d437245a58680381f
Antonio Francelino <tonybala80@gmail.com>
2017-03-24 16:29:56 -0300

    tirando as repeticoes

--- norep.c
+++ norep.c
@@ -10,30 +10,40 @@ void imprime(int num[6]);
 int main()
 {
     int  num[6];
+
     srand( time(NULL));
 
     printf("tabela com os numero sem repeticao de 1 a 6\n");
     printf("a tabela se repetira por 100 vezes\n\n");
 
-    
     norep(num);
-   /* for(i=0;i<6;i++)
-    {
-        printf("|%d\t",num[i]);
 
-    }
-    printf("FIM\n");*/
     return 0;
 }
 
 void norep(int num[6])
 {
-    int j;
+    int j,i;
+
+    for(j=0;j<6;j++) /*zerando o vetor de numeros aleatorios*/
+    {
+        num[j]=0;
+    }
 
     for(j=0;j<6;j++)
     {    
 
         num[j]=sorteio();
+        for(i=0;i<j;i++)/*tirando a repeticao*/
+        {
+            if(num[i]==num[j])
+            {
+                j--;
+                break;
+            }
+
+        }
+
     }
     imprime(num);
     return;
@@ -42,7 +52,7 @@ int sorteio()
 {
     int num;
     num= (rand()%6)+1;
-    
+
     return num;
 }
 void imprime(int num[6])
